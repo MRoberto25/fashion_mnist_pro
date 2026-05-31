7af1401e6b815104f9ac711a4a0629f3a3c8649e
MRoberto25 <robi2005d@gmail.com>
2026-05-31 22:21:58 +0300

    Update 02_train_pipeline.py

--- 02_train_pipeline.py
+++ 02_train_pipeline.py
@@ -1,37 +1,45 @@
+%%writefile 02_train_pipeline.py
 import os
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers, models
 
-# Creăm arhitectura ResNet
 class ResidualBlock(layers.Layer):
     def __init__(self, filters, stride=1, **kwargs):
         super(ResidualBlock, self).__init__(**kwargs)
+        self.stride = stride
         self.conv1 = layers.Conv2D(filters, 3, strides=stride, padding="same", use_bias=False)
         self.bn1 = layers.BatchNormalization()
         self.conv2 = layers.Conv2D(filters, 3, strides=1, padding="same", use_bias=False)
         self.bn2 = layers.BatchNormalization()
         
-        self.shortcut = models.Sequential()
-        if stride != 1:
-            self.shortcut.add(layers.Conv2D(filters, 1, strides=stride, use_bias=False))
-            self.shortcut.add(layers.BatchNormalization())
+        # Only create these layers if we actually need to change the shape
+        if self.stride != 1:
+            self.shortcut_conv = layers.Conv2D(filters, 1, strides=stride, use_bias=False)
+            self.shortcut_bn = layers.BatchNormalization()
 
     def call(self, inputs):
         x = self.conv1(inputs)
         x = self.bn1(x)
-        x = layers.ReLU()(x) # FIX: Folosim layers.ReLU() in loc de tf.nn.relu()
+        x = layers.ReLU()(x)
         x = self.conv2(x)
         x = self.bn2(x)
-        shortcut = self.shortcut(inputs)
+        
+        # If shape changes, apply shortcut layers. Otherwise, just pass inputs directly.
+        if self.stride != 1:
+            shortcut = self.shortcut_conv(inputs)
+            shortcut = self.shortcut_bn(shortcut)
+        else:
+            shortcut = inputs 
+            
         x = layers.add([x, shortcut])
-        return layers.ReLU()(x) # FIX: Folosim layers.ReLU()
+        return layers.ReLU()(x)
 
 def build_advanced_resnet(input_shape=(28, 28, 1), num_classes=10):
     inputs = layers.Input(shape=input_shape)
     x = layers.Conv2D(32, 3, padding="same", use_bias=False)(inputs)
     x = layers.BatchNormalization()(x)
-    x = layers.ReLU()(x) # FIX: Folosim layers.ReLU()
+    x = layers.ReLU()(x)
     
     x = ResidualBlock(32)(x)
     x = ResidualBlock(64, stride=2)(x)
